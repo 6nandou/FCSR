577219029aa7e3f97170c84da39bf6a771a809b8
Feña <87620680+6nandou@users.noreply.github.com>
2026-02-03 02:19:04 -0300

    Update build.gradle.kts

--- AnimeId/build.gradle.kts
+++ AnimeId/build.gradle.kts
@@ -3,5 +3,10 @@ import com.lagradost.cloudstream3.gradle.CloudstreamExtension
 cloudstream {
     authors = listOf("6nandou")
     language = "en"
-    description = "Provider for AnimeIdHentai"
+    description = "animeh"
+    status = 1
+    tvTypes = listOf(
+        "NSFW",
+    )
+    iconUrl = "https://6nandou.github.io/assets/katana.png"
 }
