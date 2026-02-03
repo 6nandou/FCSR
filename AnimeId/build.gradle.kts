28f2b7ab397678d8587553da51eb2f6f2cc1cc1e
Feña <87620680+6nandou@users.noreply.github.com>
2026-02-03 01:51:07 -0300

    Update build.gradle.kts

--- AnimeId/build.gradle.kts
+++ AnimeId/build.gradle.kts
@@ -1,15 +1,7 @@
 import com.lagradost.cloudstream3.gradle.CloudstreamExtension
 
 cloudstream {
-    // Esto llenará correctamente el plugins.json
     authors = listOf("6nandou")
     language = "en"
     description = "Provider for AnimeIdHentai"
-    
-    // Forzamos que el nombre en la lista sea el correcto
-    setMetadata(
-        "name" to "AnimeIdHentai",
-        "version" to 190,
-        "tvTypes" to listOf("NSFW")
-    )
 }
