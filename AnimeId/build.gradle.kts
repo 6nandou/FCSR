0ddbb23d3cf73128cadb6064bf8bc534cbfcfbb4
Feña <87620680+6nandou@users.noreply.github.com>
2026-02-02 22:31:14 -0300

    Update build.gradle.kts

--- AnimeId/build.gradle.kts
+++ AnimeId/build.gradle.kts
@@ -1,19 +1,13 @@
+import com.lagradost.cloudstream3.gradle.CloudstreamExtension
+
 version = 1
 
 cloudstream {
-    name        = "AnimeId"
-    authors     = listOf("6nandou")
-    language    = "es"
-    description = "Proveedor para AnimeId"
-
-    /**
-     * Status int as the following:
-     * 0: Down
-     * 1: Ok
-     * 2: Slow
-     * 3: Beta only
-    **/
-    status  = 1 
+    name = "AnimeId" 
+    authors = listOf("6nandou")
+    language = "es"
+    description = "Proveedor para Animeh"
+    status = 1
     tvTypes = listOf("NSFW")
     iconUrl = "https://avatars.githubusercontent.com/u/87620680?v=4"
 }
