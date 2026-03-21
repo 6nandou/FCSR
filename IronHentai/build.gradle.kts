9b17399a1a3a6c137d04067819237664a4c48f44
Feña <87620680+6nandou@users.noreply.github.com>
2026-03-21 18:37:09 -0300

    Create build.gradle.kts

---
+++ IronHentai/build.gradle.kts
@@ -0,0 +1,14 @@
+import com.lagradost.cloudstream3.gradle.CloudstreamExtension
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+cloudstream {
+    authors = listOf("6nandou")
+    language = "es"
+    description = "Proveedor IronHentai"
+    version = "1.0.0"
+    status = 1
+    tvTypes = listOf(
+          "NSFW"
+    )
+    iconUrl = "http://www.google.com/s2/favicons?domain=https://ironhentai.com/"
+}
