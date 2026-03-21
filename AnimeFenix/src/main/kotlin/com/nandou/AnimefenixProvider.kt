036365cf1cdb944bba5e35bcb2bf052bfe1999e2
Feña <87620680+6nandou@users.noreply.github.com>
2026-03-21 15:03:21 -0300

    Update AnimefenixProvider.kt

--- AnimeFenix/src/main/kotlin/com/nandou/AnimefenixProvider.kt
+++ AnimeFenix/src/main/kotlin/com/nandou/AnimefenixProvider.kt
@@ -7,7 +7,7 @@ import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.newExtractorLink
 
 
-class Animefenix : MainAPI() {
+class AnimefenixProvider : MainAPI() {
     override var mainUrl = "https://animefenix2.tv/"
     override var name = "AnimeFenix"
     override val hasQuickSearch = true
