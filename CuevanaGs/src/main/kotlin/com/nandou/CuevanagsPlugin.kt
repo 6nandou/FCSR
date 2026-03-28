b1e58659016a7351dd3fc049e824e561576a04e2
Feña <87620680+6nandou@users.noreply.github.com>
2026-03-28 19:33:32 -0300

    Update CuevanagsPlugin.kt

--- CuevanaGs/src/main/kotlin/com/nandou/CuevanagsPlugin.kt
+++ CuevanaGs/src/main/kotlin/com/nandou/CuevanagsPlugin.kt
@@ -1,12 +1,12 @@
-package com.nandou
+package com.lagradost.cloudstream3.movieproviders
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class CuevanaPlugin: Plugin() {
+class CuevanaGsPlugin: CloudstreamPlugin() {
     override fun load(context: Context) {
-        registerMainAPI(CuevanaProvider())
+        registerMainAPI(CuevanaGsProvider())
     }
 }
