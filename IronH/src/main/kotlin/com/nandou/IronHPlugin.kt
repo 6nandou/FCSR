1703282bd7041f90d912582e9bc70d617c3c663c
Feña <87620680+6nandou@users.noreply.github.com>
2026-05-23 23:16:38 -0400

    Create IronHPlugin.kt

---
+++ IronH/src/main/kotlin/com/nandou/IronHPlugin.kt
@@ -0,0 +1,12 @@
+package com.nandou
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class IronHPlugin: Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(IronHentaiProvider())
+    }
+}
